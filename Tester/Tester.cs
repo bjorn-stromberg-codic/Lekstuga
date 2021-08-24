5c137f5172dc9c3296a38f47b4e8565a8b556b92
Björn Strömberg <bjorn.stromberg@codic.se>
2021-08-24 15:29:50 +0200

    Add project files.

---
+++ Tester/Tester.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace Tester
+{
+    public class Tester
+    {
+        [Fact]
+        public void AlwaysSucceed()
+        {
+            Assert.True(true);
+            Assert.Equal(2, 2);
+        }
+        [Fact]
+        public void AlwaysFail()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestaVariabler()
+        {
+            string name;
+            bool lever = false;
+
+            if (lever)
+            {
+                name = "Mr Smith";
+            }
+            else
+            {
+                name = "Björn Strömberg Per Gunnar";
+            }
+
+            Assert.Equal("Björn Strömberg Per Gunnar", name);
+        }
+    }
+}
