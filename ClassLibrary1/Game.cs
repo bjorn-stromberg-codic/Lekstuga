54070087391b7b4a66441271388c3b3c67248bfe
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-14 15:34:23 +0200

    [Red] - TestGutterGame

---
+++ ClassLibrary1/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestProject1/BowlingGameTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Xunit;
+
+namespace TestProject1
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void TestGutterGame()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.Equal(0, game.Score());
+        }
+    }
+}
