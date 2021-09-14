7ba49f9c4f55e1fa0d3c88877b5f2a7a199acc92
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-14 15:44:37 +0200

    [Green] TestAllOnes

---
+++ TestProject1/LoginTests.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject1
+{
+    public class LoginTests
+    {
+        [Fact]
+        private void TestRegisterUser()
+        {
+            LoginManager loginManager = new LoginManager();
+
+            loginManager.RegisterNewUser("user", "pwd");
+        }
+    }
+}
