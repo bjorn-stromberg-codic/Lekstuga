d81c1909126f459793074ab2e69257866d575994
Björn Strömberg <bjorn.stromberg@codic.se>
2021-09-14 15:39:34 +0200

    [Red] TestAllOnes

--- TestProject1/BowlingGameTests.cs
+++ TestProject1/BowlingGameTests.cs
@@ -22,5 +22,18 @@ namespace TestProject1
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.Equal(20, game.Score());
+        }
     }
 }
